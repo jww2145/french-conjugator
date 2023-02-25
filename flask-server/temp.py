51c7eed103c3bcb160b25fb3f2eb7d06913136a5
Brian Echavarria <brianechavarria@Brians-MacBook-Pro-33.local>
2023-02-25 16:11:03 -0500

    created delete function

--- flask-server/temp.py
+++ flask-server/temp.py
@@ -13,22 +13,22 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("""CREATE TABLE words (
-		id int NOT NULL AUTO_INCREMENT,
-		indicatif varchar(99),
-		indicatif_present varchar(99),
-		indicatif_passeSimple varchar(99),
-		indicatif_imparfait varchar(99),
-		indicatif_passeCompose varchar(99),
-		indicatif_futurSimple varchar(99),
-		indicatif_passeAnterieur varchar(99),
-		indicatif_plusQueParfait varchar(99),
-		indicatif_futurAnterieur varchar (99),
-		subjonctif_present varchar(99),
-		subjonctif_passe varchar(99),
-		subjonctif_imparfait varchar(99),
-		subjonctif_plusQueParfait varchar(99),
-		conditionnel_present varchar(99),
-		conditionnel_passe1reForme varchar(99),
-		conditionnel_passe2eForme varchar(99),
-		PRIMARY KEY (id))""")
+# mycursor.execute("""CREATE TABLE words (
+# 		id int NOT NULL AUTO_INCREMENT,
+# 		infinitif varchar(99),
+# 		indicatif_present varchar(99),
+# 		indicatif_passeSimple varchar(99),
+# 		indicatif_imparfait varchar(99),
+# 		indicatif_passeCompose varchar(99),
+# 		indicatif_futurSimple varchar(99),
+# 		indicatif_passeAnterieur varchar(99),
+# 		indicatif_plusQueParfait varchar(99),
+# 		indicatif_futurAnterieur varchar (99),
+# 		subjonctif_present varchar(99),
+# 		subjonctif_passe varchar(99),
+# 		subjonctif_imparfait varchar(99),
+# 		subjonctif_plusQueParfait varchar(99),
+# 		conditionnel_present varchar(99),
+# 		conditionnel_passe1reForme varchar(99),
+# 		conditionnel_passe2eForme varchar(99),
+# 		PRIMARY KEY (id))""")

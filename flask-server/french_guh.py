a8a8b043ec5ed979c98ccdd942161b40888f9481
Brian Echavarria <brianechavarria@Brians-MacBook-Pro-33.local>
2023-02-25 12:40:21 -0500

    updating api call

--- flask-server/french_guh.py
+++ flask-server/french_guh.py
@@ -3,16 +3,22 @@ import json
 import mysql.connector
 from config import headers
 
-url = "https://french-conjugaison.p.rapidapi.com/conjugate/aller"
+# url = "https://french-conjugaison.p.rapidapi.com/conjugate/aller"
 
-response = requests.request("GET", url, headers=headers)
-text = response.text
-jsonText = (json.loads(text))["data"]
+# response = requests.request("GET", url, headers=headers)
+# text = response.text
+# jsonText = (json.loads(text))["data"]
 
-infinitive = jsonText["infinitive"]
-participe = jsonText["participe"]
+# infinitive = jsonText["infinitive"]
+# participe = jsonText["participe"]
 
+def finder(inf, form ,tense):
+	url = "https://french-conjugaison.p.rapidapi.com/conjugate/" + inf
+	response = requests.request("GET", url, headers=headers)
+	text = response.text
+	jsonText = (json.loads(text))["data"]
+	out = jsonText[form][tense]
 
+	return out
 
-print(infinitive, participe)
 

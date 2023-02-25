af12abbc047b6f28ab20f11b68dbe65aa192397d
Thomas <tdg2121@columbia.edu>
2023-02-25 16:57:11 -0500

    update method tentatively done

--- flask-server/server.py
+++ flask-server/server.py
@@ -14,14 +14,62 @@ CORS(app)
 def getConjugations(inf, form ,tense):
 	return finder(inf, form ,tense)
 
-@app.route('/hello', methods=['POST'])
+def parseConjugations(inf, form, tense):
+	out = getConjugations(inf, form, tense)
+	list = out.values()
+	string = ""
+	for i in list:
+		string = string + ", " + list[i]
+	string = string[:-2]
+	return string
+    
 
+@app.route('/hello', methods=['POST'])
+def update(word):
+	mydb = mysql.connector.connect(
+		host="localhost",
+		user=my_user,
+		password=my_pass,
+		database=my_db,
+		auth_plugin='mysql_native_password'
+	)
+	mycursor = mydb.cursor()
+	value2 = word
+	value3 = parseConjugations(word,indicatif,present)
+	value4 = parseConjugations(word, indicatif, passeSimple)
+	value5 = parseConjugations(word, indicatif, imparfait)
+	value6 = parseConjugations(word, indicatif, passeCompose)
+	value7 = parseConjugations(word, indicatif, futurSimple)
+	value8 = parseConjugations(word, indicatif, passeAnterieur)
+	value9 = parseConjugations(word, indicatif, plusQueParfait)
+	value10 = parseConjugations(word, indicatif, futurAnterieur)
+	value11 = parseConjugations(word, subjonctif, present)
+	value12 = parseConjugations(word, subjonctif, passe)
+	value13 = parseConjugations(word, subjonctif, imparfait)
+	value14 = parseConjugations(word, subjonctif, plusQueParfait)
+	value15 = parseConjugations(word, conditionnel, present)
+	value16 = parseConjugations(word, conditionnel, passe1reForme)
+	value17 = parseConjugations(word, conditionnel, passe2eForme)
+
+	
+	command = "INSERT INTO words" \
+			  "VALUES (" + value2 + ", " + value3 + ", " + value4 + ", " + value5 + ", " + value6 + ", " + value7 + ", " + value8 + ", " + value9 + ", " + value10 + ", " + value11 + ", " + value12 + ", " + value13 + ", " + value14 + ", " + value15 + ", " + value16 + ", " + value17"" 
+	mycursor.execute(command)
+	
+	
+	
+	
+	
+	
+	
+	
+	
 def create(word):
 	TABLES = {}
-	TABLES['word'] = (
-		"CREATE TABLE word ("
+	TABLES['words'] = (
+		"CREATE TABLE words ("
 		"   id int NOT NULL AUTO_INCREMENT"
-		"   indicatif varchar(99)"
+		"   infinitif varchar(99)"
 		"	indicatif_present varchar(99)"
 		"   indicatif_passeSimple varchar(99)"
 		"	indicatif_imparfait varchar(99)"
@@ -37,7 +85,7 @@ def create(word):
 		"   conditionnel_present varchar(99)"
 		"   conditionnel_passe1reForme varchar(99)"
 		"   conditionnel_passe2eForme varchar(99)"
-		"   PRIMARY KEY (indicatif)"
+		"   PRIMARY KEY (id)"
 	)
 
 

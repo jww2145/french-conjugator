25ec9b48f0c7a73a0a98a14575a49a57c4588218
Brian Echavarria <brianechavarria@Brians-MacBook-Pro-33.local>
2023-02-25 11:56:26 -0500

    added read

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,22 +1,26 @@
 # IMPORT
 from flask import Flask
 from flask_cors import CORS
+from french_guh import finder
 # APP SETUP
 app = Flask(__name__)
 # enable resource sharing between frontend and server
 CORS(app)
+
 # ROUTES
-@app.route('/hello', methods=['GET'])
-def getHello():
-	return 'This is a GET request!'
+@app.route('/read', methods=['GET'])
+def getConjugations(inf, form ,tense):
+	return finder(inf, form ,tense)
+
 @app.route('/hello', methods=['POST'])
 def postHello():
 	return 'This is a POST request!'
-@app.route('/hello', methods=['PUT'])
-def putHello():
-	return 'This is a PUT request!'
+
+
 @app.route('/hello', methods=['DELETE'])
 def deleteHello():
 	return 'This is a DELETE request!'
+
+
 if __name__ == "__main__":
     app.run(debug=True)

1921c3d0d646fc9a562cc1d3df67d1fa90b44b62
Brian Echavarria <brianechavarria@Brians-MacBook-Pro-33.local>
2023-02-25 17:32:57 -0500

    ???

--- flask-server/server.py
+++ flask-server/server.py
@@ -37,7 +37,7 @@ def update(word):
 	test = "SELECT COUNT(*) FROM words WHERE indicitif=" + word
 	mycursor.execute(test)
 
-	if mycursor.rowcount != 0:
+	if mycursor.rowcount == 0:
 		value2 = word
 		value3 = parseConjugations(word,"indicatif","present")
 		value4 = parseConjugations(word, "indicatif", "passeSimple")
@@ -107,35 +107,35 @@ def deleteHello():
 
 
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user=my_user,
-  password=my_pass,
-  database=my_db,
-  auth_plugin='mysql_native_password'
-)
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user=my_user,
+#   password=my_pass,
+#   database=my_db,
+#   auth_plugin='mysql_native_password'
+# )
 
-mycursor = mydb.cursor()
+# mycursor = mydb.cursor()
 
-mycursor.execute("""CREATE TABLE words (
-		id int NOT NULL AUTO_INCREMENT,
-		infinitif varchar(99),
-		indicatif_present varchar(99),
-		indicatif_passeSimple varchar(99),
-		indicatif_imparfait varchar(99),
-		indicatif_passeCompose varchar(99),
-		indicatif_futurSimple varchar(99),
-		indicatif_passeAnterieur varchar(99),
-		indicatif_plusQueParfait varchar(99),
-		indicatif_futurAnterieur varchar (99),
-		subjonctif_present varchar(99),
-		subjonctif_passe varchar(99),
-		subjonctif_imparfait varchar(99),
-		subjonctif_plusQueParfait varchar(99),
-		conditionnel_present varchar(99),
-		conditionnel_passe1reForme varchar(99),
-		conditionnel_passe2eForme varchar(99),
-		PRIMARY KEY (id))""")
+# mycursor.execute("""CREATE TABLE words (
+# 		id int NOT NULL AUTO_INCREMENT,
+# 		infinitif varchar(99),
+# 		indicatif_present varchar(99),
+# 		indicatif_passeSimple varchar(99),
+# 		indicatif_imparfait varchar(99),
+# 		indicatif_passeCompose varchar(99),
+# 		indicatif_futurSimple varchar(99),
+# 		indicatif_passeAnterieur varchar(99),
+# 		indicatif_plusQueParfait varchar(99),
+# 		indicatif_futurAnterieur varchar (99),
+# 		subjonctif_present varchar(99),
+# 		subjonctif_passe varchar(99),
+# 		subjonctif_imparfait varchar(99),
+# 		subjonctif_plusQueParfait varchar(99),
+# 		conditionnel_present varchar(99),
+# 		conditionnel_passe1reForme varchar(99),
+# 		conditionnel_passe2eForme varchar(99),
+# 		PRIMARY KEY (id))""")
 
 
 if __name__ == "__main__":

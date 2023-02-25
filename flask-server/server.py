fe269c558b4534ef33d0f3fddcd76785a49a55a8
jww2145 <jww2145@columbia.edu>
2023-02-25 10:58:35 -0500

    made routes for backend

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
+# IMPORT
+from flask import Flask
+from flask_cors import CORS
+# APP SETUP
 app = Flask(__name__)
-
-@app.route('/')
-
-def index():
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
+# enable resource sharing between frontend and server
+CORS(app)
+# ROUTES
+@app.route('/hello', methods=['GET'])
+def getHello():
+	return 'This is a GET request!'
+@app.route('/hello', methods=['POST'])
+def postHello():
+	return 'This is a POST request!'
+@app.route('/hello', methods=['PUT'])
+def putHello():
+	return 'This is a PUT request!'
+@app.route('/hello', methods=['DELETE'])
+def deleteHello():
+	return 'This is a DELETE request!'
+if __name__ == "__main__":
     app.run(debug=True)
